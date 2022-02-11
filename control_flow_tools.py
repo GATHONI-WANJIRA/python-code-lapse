9d5a5009849cdb87ffbb99f07ed55da940483291
GATHONI-WANJIRA <janetgathoni910@gmail.com>
2022-02-11 18:01:03 +0300

    modified code

--- control_flow_tools.py
+++ control_flow_tools.py
@@ -1,7 +1,7 @@
-#continue in loops.
-
+#Continue statements.
 for num in range (2,10):
-    if num % 2 == 0:
-        print("this is an even number", num)
-        continue
-    print("this is an odd number" , num)
+     if num % 2 == 0:
+         print("this is an even number", num)
+         continue
+     print("this is an odd number" , num)
+    
