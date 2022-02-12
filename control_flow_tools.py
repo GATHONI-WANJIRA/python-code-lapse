81c83b65ff83591b7f871319d74e2342dc77ad66
GATHONI-WANJIRA <janetgathoni910@gmail.com>
2022-02-12 14:04:23 +0300

    added new file

--- control_flow_tools.py
+++ control_flow_tools.py
@@ -1,53 +1,5 @@
-#If, elif, and else statements.
+#pass statements 
 
-def get_capital_city(country):
-    if country == "kenya":
-        return "Nairobi"
-    elif country == "DRC":
-        return "kinshasa"
-    elif country == "lebanon":
-        return "Beruit"
-    else: 
-         return "None"
+class interns:
+    pass
 
-         #for loops.
-names = ["noni", "wahu", "vivo"]  
-for w in names:
-    print (w, len(w))
-
-    # While loops.
-Days = 5
-while Days > 4:
-    print ("enough")
-else: 
-    print ("add some more")
-
-
-#compilation of For and else break loops.
-for n in range (2, 10):
-    # print (n)
-    for x in range (2, n):
-        # print (x)
-
-       if n % x == 0:
-           print (n," is not a prime number!")
-           break
-    else:
-            print (n," is a prime number")
-
-for num in range (2,10):
-    if num % 2 == 0:
-        print("this is an even number", num)
-        continue
-    print("this is an odd number" , num)
-
-
-
-
-#Continue statements.
-for num in range (2,10):
-     if num % 2 == 0:
-         print("this is an even number", num)
-         continue
-     print("this is an odd number" , num)
-    
