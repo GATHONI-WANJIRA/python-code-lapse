c9b52b5d60b8c4fc8b01283862636c9b0a9a4c1c
GATHONI-WANJIRA <janetgathoni910@gmail.com>
2022-02-13 08:47:43 +0300

    modified code

--- control_flow_tools.py
+++ control_flow_tools.py
@@ -1,5 +1,13 @@
-#pass statements 
-
-class interns:
-    pass
-
+#match statements
+number =int(input("code"))
+def country_name(code):
+    match code:
+      case 254:
+        return ("Kenya")                                                                   
+      case 256:
+          return ("Uganda")
+      case 247:
+          return ("tanzania")
+      case _:
+          return ("Notfound")
+print (country_name(number) )
