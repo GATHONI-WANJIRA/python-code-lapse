d60f2f099eb00c45bb46821460657a260ebf0f77
GATHONI-WANJIRA <janetgathoni910@gmail.com>
2022-02-18 20:14:55 +0300

    updated fibonacci files

--- control_flow_tools.py
+++ control_flow_tools.py
@@ -1,1 +1,20 @@
-#statements 
+#fibonacci series.
+
+nterms = int(input("How many terms ?"))
+#first two terms.
+a,b= 0,1
+count = 0
+if nterms <= 0:
+    print("Please enter a positive integer.")
+elif nterms == 1:
+    print ("fibonacci sequence upto" , nterms, ":")
+    print (a)
+else:
+    print ("fibonacci sequence:")
+    while count < nterms:
+        print (a)
+        nth = a + b
+        a = b
+        b = nth
+        count += 1
+
