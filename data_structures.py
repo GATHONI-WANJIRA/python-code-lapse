f74961d5110207502c19355cd83edd774188927e
GATHONI-WANJIRA <janetgathoni910@gmail.com>
2022-02-24 21:10:32 +0300

    Nested list comprehensions

--- data_structures.py
+++ data_structures.py
@@ -1,9 +1,10 @@
-squares = []
-for x in range (10):
-    squares.append (x**2)
-    print (squares)
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
 
-
-
-
-
+transposed = []
+for i in range(4):
+     transposed.append([row[i] for row in matrix])
+     print (transposed)
