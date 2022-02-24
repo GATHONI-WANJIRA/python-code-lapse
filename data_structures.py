f83799e9b006b0eb73e9414375dda4670ff31fc3
GATHONI-WANJIRA <janetgathoni910@gmail.com>
2022-02-24 22:53:02 +0300

    Tuples and Sequences

--- data_structures.py
+++ data_structures.py
@@ -1,3 +1,9 @@
-values = [1,2,3,1,5,7]
-del values [3]
-print (values)
+countries  = (+254 , "nigeria" , +976)
+countries[1]
+print (countries [1])
+
+
+#tuples can also be nested.
+cities = ("Nairobi", "arusha","Beruit")
+places = countries + cities
+print (places)
