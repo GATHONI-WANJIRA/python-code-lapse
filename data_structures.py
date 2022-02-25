9b09e185976bd1aaadb63bff6e587c52065706cd
GATHONI-WANJIRA <janetgathoni910@gmail.com>
2022-02-25 18:48:21 +0300

    The String format() Method

--- data_structures.py
+++ data_structures.py
@@ -1,2 +1,3 @@
-import math
-print (f'The value of pi is approximately {math.pi: 3f}.')
+table = {'Nairobi': +254, 'Tanzania': +256, 'Lebanon': +976}
+print('Nairobi: {0[Nairobi]:d}; Tanzania: {0[Tanzania]:d}; '
+      'Lebanon: {0[Lebanon]:d}'.format(table))
