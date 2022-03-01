0d5ffff68fcc487d302707af8d9b3b06dc2f627e
GATHONI-WANJIRA <janetgathoni910@gmail.com>
2022-03-01 09:29:23 +0300

    handling exceptions

--- errors_expceptions.py
+++ errors_expceptions.py
@@ -1,1 +1,6 @@
-print ("hello world")
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+       print("Oops!  That was no valid number.  Try again...")
